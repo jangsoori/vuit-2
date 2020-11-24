35a7dd00bdbc0cdac6d7af7ffb97ff10d6d8df53
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-11-24 15:07:48 +0000

    Added components to render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import SearchProvider from "./contexts/SearchContext";
+import GlobalStyle from "./styles/GlobalStyle";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <SearchProvider>
+      <App />
+    </SearchProvider>
+  </>,
+
+  document.getElementById("root")
+);
