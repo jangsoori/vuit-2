4ca5bc00736c03b384671b7c723ba75d44b80306
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-11-24 15:09:09 +0000

    Deleted import

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import React, { createRef, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 const ImageWrapper = styled.div`
