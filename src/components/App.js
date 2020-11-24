0a8f94ed5c1058f437769c4ce3b46c29381f0774
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-11-24 15:07:22 +0000

    Created App wrapper

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,22 @@
-import React from "react";
+import styled from "@emotion/styled";
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import { QueryCache, ReactQueryCacheProvider } from "react-query";
+const AppWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+`;
+const queryCache = new QueryCache();
 
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <AppWrapper>
+      <Header />
+      <ReactQueryCacheProvider queryCache={queryCache}>
+        <Main />
+      </ReactQueryCacheProvider>
+    </AppWrapper>
+  );
 }
